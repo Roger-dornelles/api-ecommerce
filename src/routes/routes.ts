c607d9b55aa9d72b57bea2462bb99f8e5e87e262
Roger <rsd.santo@outlook.com>
2023-03-17 05:38:20 -0300

    feat: created route delete product and images

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -26,5 +26,6 @@ route.get('/user/info/:id', privateRoute, userController.userInfo);
 
 route.post('/user/new/product/:id', privateRoute, uploadImages.array('photos', 10), productController.newProduct);
 route.put('/product/update/:id', privateRoute, productController.updateProductInformation);
+route.delete('/product/:id', privateRoute, productController.deleteOneProduct);
 
 export default route;
